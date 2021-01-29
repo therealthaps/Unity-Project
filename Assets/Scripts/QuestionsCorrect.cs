9fbf73b06beaf1100ba259eb7452f4f25c18a987
tblus <tgblus@knox.edu>
2021-01-29 14:39:23 -0600

    Merge branch 'Feature.Tristan' into Development
    # Conflicts:Resolved with external merge tool
    #	Assets/Scenes/SampleScene.unity

---
+++ Assets/Scripts/QuestionsCorrect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionsCorrect : MonoBehaviour
+{
+    public static int Correct = 0;
+    public static int Streak = 0;
+    Text count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count.text = Correct.ToString();
+    }
+}
---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// code taken from youtuber tutorial 
+// https://www.youtube.com/watch?v=QbqnDbexrCw&vl=en
+
+/*remainder of code and references to this code are in the player object
+ * for each correct answer up to 5: + 1000 pts per answer
+ * for each correct answer up to 10: + 2000 pts per answer
+ * for each correct answer up to 20: + 4000 pts per answer
+ * for each correct answer greater than 20: + 8000 pts per answer
+ * for each incorrect answer streak reset to 0 and 1000 pts lost
+ * */
+public class ScoringSystem : MonoBehaviour
+{
+
+    public static int Points = 0;
+    public static int strk = 0;
+    Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        strk = QuestionsCorrect.Streak;
+        score.text = Points.ToString();
+    }
+    public static void Correct()
+    {
+        if (strk <= 5)
+        {
+            Points += 1000;
+        }
+        else if (strk > 5)
+        {
+            Points += 2000;
+        }
+        else if (strk > 10) 
+        {
+            Points += 4000;
+        }
+        else if (strk > 20)
+        {
+            Points += 8000;
+        }
+        QuestionsCorrect.Correct += 1;
+        QuestionsCorrect.Streak += 1;
+    }
+
+    public static void Incorrect()
+    {
+        Points -= 1000;
+        QuestionsCorrect.Streak = 0;
+    }
+}
