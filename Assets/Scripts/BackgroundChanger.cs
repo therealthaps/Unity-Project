48508d6e0d940f122d839c9196b205b3fffbef8f
tblus <tgblus@knox.edu>
2021-01-29 18:00:37 -0600

    Adding to taskbar, adding rotating backgrounds, and scripting for same

---
+++ Assets/Scripts/BackgroundChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundChanger : MonoBehaviour
+{
+
+    private float t = Time.time;
+    private float seconds = 0;
+    public float delay = 30;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteArray[(int)Random.Range((float)0.0, spriteArray.Length)];
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds = Time.time - t;
+        if (seconds >= delay)
+        {
+            ChangeSprite();
+            t = Time.time;
+            seconds = 0;
+        }
+    }
+}
