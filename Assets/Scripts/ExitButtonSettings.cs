90bd3344656de8b3db38932de0c70ff57a2d0690
Kartikay <kbhuchar@knox.edu>
2021-02-02 14:33:52 -0600

    Settings button, player,enemy features again

---
+++ Assets/Scripts/ExitButtonSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonSettings : MonoBehaviour
+{
+    public GameObject Panel;
+    bool isPaused = false;
+    // Start is called before the first frame update
+    public void ExitSettings()
+    {
+
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+
+            Time.timeScale = 1;
+            isPaused = false;
+
+        }
+
+    }
+}
